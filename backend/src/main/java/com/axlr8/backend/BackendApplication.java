9c2074be74e3d4806fb3e487d25712ec31ce114e
Robert Aguilar <62629630+RAguilar777@users.noreply.github.com>
2023-11-18 12:23:41 -0500

    Minor changes

--- backend/src/main/java/com/axlr8/backend/BackendApplication.java
+++ backend/src/main/java/com/axlr8/backend/BackendApplication.java
@@ -1,10 +1,9 @@
 package com.axlr8.backend;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAdminServer
+
 @SpringBootApplication
 public class BackendApplication {
 
