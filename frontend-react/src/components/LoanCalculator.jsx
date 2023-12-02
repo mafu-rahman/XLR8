119ad861162f28bd963f31bf70aa3062688c3015
Mahfuz Rahman <mafu@my.yorku.ca>
2023-12-02 04:22:13 -0500

    Update LoanCalculator.jsx

--- frontend-react/src/components/LoanCalculator.jsx
+++ frontend-react/src/components/LoanCalculator.jsx
@@ -35,7 +35,7 @@ const LoanCalculator = () => {
         try {
           const queryParams = new URLSearchParams(formData).toString();
     
-          const response = await fetch(`http://0.0.0.0:8081/api/v1/loan/calculate-loan?${queryParams}`, {
+          const response = await fetch(`http://localhost:10000/api/v1/loan/calculate-loan?${queryParams}`, {
             method: 'GET',
             headers: {
               // Add any headers if needed
