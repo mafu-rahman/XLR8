05ed3ae7caf272a6ec80aa02797b94295a06cf08
Nadimul Hasan <samnad14@gmail.com>
2023-12-03 06:54:23 -0500

    Update SecurityConfiguration.java

--- backend/src/main/java/com/axlr8/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/axlr8/backend/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 @EnableWebSecurity
